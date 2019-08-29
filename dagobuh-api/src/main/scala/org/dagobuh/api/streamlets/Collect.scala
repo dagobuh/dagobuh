c22e60ae23a86fbee1e56fdb6a38237c1a2c2bbc
Timothy Bess <tdbgamer@gmail.com>
2019-08-29 15:38:13 -0400

    add collect

---
+++ dagobuh-api/src/main/scala/org/dagobuh/api/streamlets/Collect.scala
@@ -0,0 +1,29 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+package org.dagobuh.api.streamlets
+
+import org.dagobuh.api.appliers.StreamletApplier
+import org.dagobuh.api.inputstream.InputStream
+
+import scala.language.higherKinds
+import scala.reflect.ClassTag
+
+class Collect[A, B: ClassTag](func: PartialFunction[A, B]) {
+  def run[F[_]](in: InputStream[F, A]): InputStream[F, B] = {
+    in.filter(func.isDefinedAt).map(func.apply)
+  }
+}
+
+object Collect {
+  def apply[A, B: ClassTag](func: PartialFunction[A, B]): Collect[A, B] = new Collect(func)
+  implicit def collect[F[_], A, B: ClassTag]: StreamletApplier[F, Collect[A, B], A, B] = new StreamletApplier[F, Collect[A, B], A, B] {
+    override def run(in: InputStream[F, A], streamlet: Collect[A, B]): InputStream[F, B] = streamlet.run(in)
+  }
+}
+
+
+
+
+
+
