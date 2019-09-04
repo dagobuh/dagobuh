a64780760b4c1a9c857fd1f30489376568dd3c3a
Timothy Bess <tdbgamer@gmail.com>
2019-09-04 11:17:01 -0400

    union sources

--- dagobuh-api/src/main/scala/org/dagobuh/api/implicits/package.scala
+++ dagobuh-api/src/main/scala/org/dagobuh/api/implicits/package.scala
@@ -1,15 +1,23 @@
 package org.dagobuh.api
 
-import org.dagobuh.api.streamlets.{CompositeTransformer, Transformer}
+import org.dagobuh.api.inputstream.InputStream
+import org.dagobuh.api.streamlets.{CompositeTransformer, Source, Transformer}
 
 import scala.language.{higherKinds, implicitConversions}
 
 package object implicits {
   implicit def transformer2transformerOps[F[_], A, B](transformer: Transformer[F, A, B]): TransformerOps[F, A, B] = new TransformerOps(transformer)
+  implicit def source2sourceOps[F[_], A, B](source: Source[F, A]): SourceOps[F, A] = new SourceOps(source)
 
   class TransformerOps[F[_], A, B](val transformer: Transformer[F, A, B]) extends AnyVal {
     def compose[C](other: Transformer[F, C, A]): CompositeTransformer[F, A, B, C] = {
       CompositeTransformer(transformer, other)
+    }
+  }
+
+  class SourceOps[F[_], A](val source: Source[F, A]) extends AnyVal {
+    def union(other: Source[F, A]): Source[F, A] = new Source[F, A] {
+      override def run(): InputStream[F, A] = source.run().union(other.run())
     }
   }
 }
